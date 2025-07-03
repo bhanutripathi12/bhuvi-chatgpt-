de8d13dcb4754cf8b8c73198754f08e2747c32f4
BHUWNESH TRIPATHI <151540653+bhanutripathi12@users.noreply.github.com>
2025-07-03 14:31:40 +0530

    qqqq

--- my_app/app/assistant.tsx
+++ my_app/app/assistant.tsx
@@ -25,13 +25,13 @@ export const Assistant = () => {
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">
-                    Build Your Own ChatGPT UX
+                    Build Your Own ChatGPT Here
                   </BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
                   <BreadcrumbPage>
-                    Starter Template
+                    Starter 
                   </BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
