316ac12b7b3ebd253fb52ed8e25e6111f7f79555
BHUWNESH TRIPATHI <151540653+bhanutripathi12@users.noreply.github.com>
2025-07-03 14:28:12 +0530

    Update app-sidebar.tsx

--- my_app/components/app-sidebar.tsx
+++ my_app/components/app-sidebar.tsx
@@ -20,12 +20,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-                <Link href="https://assistant-ui.com" target="_blank">
+                <Link href="https://bhuviportfolio-pilm.vercel.app/" target="_blank">
                   <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                     <MessagesSquare className="size-4" />
                   </div>
                   <div className="flex flex-col gap-0.5 leading-none">
-                    <span className="font-semibold">assistant-ui</span>
+                    <span className="font-semibold">My_Portfolio</span>
                   </div>
                 </Link>
               </SidebarMenuButton>
@@ -42,13 +42,13 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
          
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <Link href="https://github.com/assistant-ui/assistant-ui" target="_blank">
+              <Link href="https://leetcode.com/u/bhanutripathi/" target="_blank">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                   <Github className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">GitHub</span>
-                  <span className="">View Source</span>
+                  <span className="font-semibold">Leetcode Url</span>
+                 
                 </div>
               </Link>
             </SidebarMenuButton>
